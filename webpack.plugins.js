76bd6ac826517e5c5ad74d606eba828164b8fb7c
Can <29177826+cankutayeser@users.noreply.github.com>
2022-02-02 00:54:12 +0300

    fix "Buffer is not defined"

--- webpack.plugins.js
+++ webpack.plugins.js
@@ -1,5 +1,9 @@
-const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
-
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
+var webpack = require("webpack");
 module.exports = [
-  new ForkTsCheckerWebpackPlugin()
+  new ForkTsCheckerWebpackPlugin(),
+  new webpack.ProvidePlugin({
+    process: "process/browser",
+    Buffer: ["buffer", "Buffer"],
+  }),
 ];
