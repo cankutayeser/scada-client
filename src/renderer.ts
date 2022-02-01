995ea8a0aa90a9ba6f235e2c0fdf1738a64cae8e
Can <29177826+cankutayeser@users.noreply.github.com>
2022-02-02 00:11:25 +0300

    add jsmodbus and readcoil renderer code

--- src/renderer.ts
+++ src/renderer.ts
@@ -25,7 +25,66 @@
  *  });
  * ```
  */
+import Modbus from "jsmodbus";
+import { Socket, SocketConnectOpts } from "net";
 
-import './index.css';
+import "./index.css";
 
-console.log('👋 This message is being logged by "renderer.js", included via webpack');
+console.log(
+  '👋 This message is being logged by "renderer.js", included via webpack'
+);
+
+const socket = new Socket();
+
+const options: SocketConnectOpts = {
+  host: "127.0.0.1",
+  port: 8502,
+};
+const client = new Modbus.client.TCP(socket);
+
+const readStart = 0;
+const readCount = 10;
+
+socket.on("connect", function () {
+  client
+    .readHoldingRegisters(readStart, readCount)
+    .then(({ metrics, request, response }) => {
+      console.log("Transfer Time: " + metrics.transferTime);
+      console.log("Response Body Payload: " + response.body.valuesAsArray);
+      console.log(
+        "Response Body Payload As Buffer: " + response.body.valuesAsBuffer
+      );
+    })
+    .catch(handleErrors)
+    .finally(() => socket.end());
+});
+
+socket.on("error", console.error);
+socket.connect(options);
+
+function handleErrors(err: any) {
+  if (Modbus.errors.isUserRequestError(err)) {
+    switch (err.err) {
+      case "OutOfSync":
+      case "Protocol":
+      case "Timeout":
+      case "ManuallyCleared":
+      case "ModbusException":
+      case "Offline":
+      case "crcMismatch":
+        console.log(
+          "Error Message: " + err.message,
+          "Error" + "Modbus Error Type: " + err.err
+        );
+        break;
+    }
+  } else if (Modbus.errors.isInternalException(err)) {
+    console.log(
+      "Error Message: " + err.message,
+      "Error" + "Error Name: " + err.name,
+      err.stack
+    );
+  } else {
+    console.log("Unknown Error", err);
+  }
+}
